8b0125ada28305ebe23aaf30b4342dfef26f169a
Jonss <joaosantana.ti@gmail.com>
2016-07-08 21:17:50 -0300

    aula 1.3

--- app/src/main/java/com/jonss/github/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/jonss/github/agenda/ListaAlunosActivity.java
@@ -1,7 +1,9 @@
 package com.jonss.github.agenda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Danzig", "Doyle", "Jerry Only", "Michalle Graves", "Robo"};
+
+        ListView listView = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listView.setAdapter(adapter);
     }
+
 }
