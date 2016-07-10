70b7ed7db50f29bdc7878a6fe3d0c1bc1224a548
Jonss <joaosantana.ti@gmail.com>
2016-07-10 14:38:01 -0300

    aula 3.3

--- app/src/main/java/com/jonss/github/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/jonss/github/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package com.jonss.github.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -17,6 +20,16 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listView.setAdapter(adapter);
+
+        Button novoAlunoButton = (Button) findViewById(R.id.novo_aluno);
+        novoAlunoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
