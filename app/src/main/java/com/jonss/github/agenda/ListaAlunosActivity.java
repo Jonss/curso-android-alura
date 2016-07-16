69823c803b24888e0350c6b8ff3a658be0faedd6
Jonss <joaosantana.ti@gmail.com>
2016-07-16 11:06:24 -0300

    aula 5.1

--- app/src/main/java/com/jonss/github/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/jonss/github/agenda/ListaAlunosActivity.java
@@ -3,10 +3,14 @@ package com.jonss.github.agenda;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jonss.github.agenda.dao.AlunoDao;
 import com.jonss.github.agenda.model.Aluno;
@@ -15,10 +19,15 @@ import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private AlunoDao alunoDao = new AlunoDao(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        listView = (ListView) findViewById(R.id.lista_alunos);
 
         Button novoAlunoButton = (Button) findViewById(R.id.novo_aluno);
         novoAlunoButton.setOnClickListener(new View.OnClickListener() {
@@ -29,16 +38,36 @@ public class ListaAlunosActivity extends AppCompatActivity {
             }
         });
 
+        registerForContextMenu(listView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        AlunoDao alunoDao = new AlunoDao(this);
+        carregaLista();
+    }
+
+    private void carregaLista() {
         List<Aluno> alunos = alunoDao.getAll();
 
-        ListView listView = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(final ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("deletar");
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listView.getItemAtPosition(info.position);
+                alunoDao.deleta(aluno);
+                Toast.makeText(ListaAlunosActivity.this, aluno.getNome() + " deletado", Toast.LENGTH_SHORT).show();
+                carregaLista();
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/jonss/github/agenda/dao/AlunoDao.java
+++ app/src/main/java/com/jonss/github/agenda/dao/AlunoDao.java
@@ -72,4 +72,11 @@ public class AlunoDao extends SQLiteOpenHelper {
         }
         return alunos;
     }
+
+    public void deleta(Aluno aluno) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        String[] params = {aluno.getId().toString() };
+        db.delete(TABLE, "id = ?", params);
+    }
 }
