a5c5d5ca82987860f71fa1557d29938910c3bbd3
Jonss <joaosantana.ti@gmail.com>
2016-07-20 22:35:01 -0300

    aula 2.2

--- app/src/main/java/com/jonss/github/agenda/FormularioActivity.java
+++ app/src/main/java/com/jonss/github/agenda/FormularioActivity.java
@@ -1,16 +1,23 @@
 package com.jonss.github.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.jonss.github.agenda.dao.AlunoDao;
 import com.jonss.github.agenda.helper.FormularioHelper;
 import com.jonss.github.agenda.model.Aluno;
+
+import java.io.File;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -25,10 +32,21 @@ public class FormularioActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
-        if(aluno != null) {
+        if (aluno != null) {
             mFormularioHelper.setAluno(aluno);
         }
 
+        Button botaoCamera = (Button) findViewById(R.id.formulario_camera_button);
+        botaoCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                String caminhoFoto = getExternalFilesDir(null) + "/ " + System.currentTimeMillis() + " .jpg";
+                File file = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivity(intentCamera);
+            }
+        });
     }
 
 
@@ -45,7 +63,7 @@ public class FormularioActivity extends AppCompatActivity {
             case R.id.menu_formulario_ok:
                 Aluno aluno = mFormularioHelper.getAluno();
                 AlunoDao alunoDao = new AlunoDao(this);
-                if(aluno.getId() == null) {
+                if (aluno.getId() == null) {
                     alunoDao.create(aluno);
                 } else {
                     alunoDao.update(aluno);
@@ -57,8 +75,6 @@ public class FormularioActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
 
 
 }
